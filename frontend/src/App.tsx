3a524006add8c18a5b784dbdd4b602cdfe96e776
c-riq <chrisrieck6@gmail.com>
2025-03-04 15:00:46 +0100

    Frontend scaffolding

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button, Container, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h1">Hello, World!</Typography>
+      <Button variant="contained">Click me</Button>
+    </Container>
+  );
+}
+
+export default App;
