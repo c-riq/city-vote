8fb1e77c6c8722770cbbac7c5d337c09fae68c4a
c-riq <chrisrieck6@gmail.com>
2025-05-18 11:01:39 +0200

    Update UI usage of coordinates

--- frontend/src/backendTypes.ts
+++ frontend/src/backendTypes.ts
@@ -25,10 +25,8 @@ export interface AutocompleteResponse {
         countryCode: string;
         population?: number;
         populationDate?: string;
-        coordinates?: {
-            latitude: number;
-            longitude: number;
-        };
+        latitude?: number;
+        longitude?: number;
         officialWebsite?: string;
         socialMedia?: {
             twitter?: string;
